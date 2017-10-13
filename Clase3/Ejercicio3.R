112e3c3326325e4e2bdbb132afc4656afefa4553
derek-corcoran-barrios <gaiterodelsurdelmundo@gmail.com>
2017-10-13 15:25:37 -0300

    <Ejercicio 3 clase 3 listo>

--- Clase3/Ejercicio3.R
+++ Clase3/Ejercicio3.R
@@ -6,7 +6,12 @@ library(dplyr)
 library(stringr)
 #para generar las fechas
 library(lubridate)
+####Leer la pestaña estaciones Meteorologicas
+EM <- read_excel("~/Downloads/estado-aire80422dea683e61618024ff030098b759.xlsx", sheet = "T001")
+#Cambiar el nombre de la columna 1 para que sea igual que las columnas de la temperatura y humedad
+colnames(EM)[1] <- "Est_Meteoro"
 
+########################################################################
 ####Leer la pestaña de la temperatura media
 TempMedia <- read_excel("~/Downloads/estado-aire80422dea683e61618024ff030098b759.xlsx", sheet = "E10000003")
 
@@ -28,6 +33,15 @@ TempMedia$Fecha <- str_replace(TempMedia$Fecha, "_00", "_01")
 
 TempMedia$Fecha <- ymd(TempMedia$Fecha)
 
+#Generar las columas para mes y año
+
+TempMedia$Year <- year(TempMedia$Fecha)
+
+TempMedia$mes <- month(TempMedia$Fecha)
+
+TempMedia <- left_join(TempMedia, EM) %>% select(Fecha, Year, mes, TempMedia, Ciudad_localidad)
+
+
 ##############################################################################################3
 ####Leer la pestaña Humedad Media
 HumMedia <- read_excel("~/Downloads/estado-aire80422dea683e61618024ff030098b759.xlsx", sheet = "E10000006")
@@ -43,5 +57,45 @@ colnames(HumMedia)[4] <- "HumMedia"
 
 HumMedia <- HumMedia[!str_detect(HumMedia$Fecha, "_13"),]
 
-####Leer la pestaña estaciones Meteorologicas
-EM <- read_excel("~/Downloads/estado-aire80422dea683e61618024ff030098b759.xlsx", sheet = "T001")
+#Remplazar dias 00, por 01
+
+HumMedia$Fecha <- str_replace(HumMedia$Fecha, "_00", "_01")
+
+##Transformar en formato Fecha
+
+HumMedia$Fecha <- ymd(HumMedia$Fecha)
+
+#Generar las columas para mes y año
+
+HumMedia$Year <- year(HumMedia$Fecha)
+
+HumMedia$mes <- month(HumMedia$Fecha)
+
+HumMedia <- left_join(HumMedia, EM) %>% select(Fecha, Year, mes, HumMedia, Ciudad_localidad)
+
+TempHum <- full_join(TempMedia, HumMedia)
+
+saveRDS(TempHum, "TempHum.rds")
+
+TempHum <- readRDS("TempHum.rds")
+
+PA <- TempHum %>% filter(Ciudad_localidad == "Punta Arenas")
+
+PAySan <- TempHum %>% filter(Ciudad_localidad == "Punta Arenas" | Ciudad_localidad == "Quinta Normal")
+
+ggplot(PA, aes(x = mes, y = TempMedia)) + geom_point()
+ggplot(PA, aes(x = mes, y = TempMedia)) + geom_point() + geom_smooth()
+ggplot(PA, aes(x = mes, y = TempMedia)) + geom_point() +  stat_smooth(method = "lm", formula = y ~ x + I(x^2)) 
+
+library(tidyr)
+
+San <- TempHum %>% filter(Ciudad_localidad == "Quinta Normal")
+
+SATH <- gather(San, key = Unidad, value = medida, TempMedia, HumMedia)
+
+ggplot(SATH, aes(x = mes, y = medida)) + geom_point() +  stat_smooth(method = "lm", formula = y ~ x + I(x^2), aes(fill = Unidad, color = Unidad)) 
+
+ggplot(PAySan, aes(x = mes, y = TempMedia)) + geom_point() +  stat_smooth(method = "lm", formula = y ~ x + I(x^2), aes(fill = Ciudad_localidad, color = Ciudad_localidad)) 
+
+
+ggplot(PAySan, aes(x = mes, y = TempMedia)) + geom_point() +  stat_smooth(method = "lm", formula = y ~ x + I(x^2)) + facet_grid(Ciudad_localidad ~ .)
